4ecfbd7df1442eebf42103be3a29bf32ebef3fb2
kimjimin <hawoosay2@gmail.com>
2025-05-20 13:09:04 +0900

    ai분석피드백추가

--- lib/statistics/ai_analysis/ai_analysis_home.dart
+++ lib/statistics/ai_analysis/ai_analysis_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart'; //이모지
 import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,10 +8,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<Map<String, dynamic>> fetchMonthlyReport(String userId) async {
   final response = await http.get(Uri.parse('http://10.0.2.2:8080/note/report/monthly/$userId'));
   print("요청 URL: http://<서버주소>/note/report/monthly/$userId");
-  print("상태 코드: ${response.statusCode}");
-  print("응답 본문: ${response.body}");
+
   if(response.statusCode==200){
     final decoded = utf8.decode(response.bodyBytes);
+    print("상태 코드: ${response.statusCode}");
+    print("응답 본문: ${response.body}");
     return jsonDecode(decoded);
   }else{
     throw Exception("월간 리포트 불러오기 실패 : ${response.body}");
@@ -47,6 +49,22 @@ class _AIAnalysisHomePageState extends State<AIAnalysisHomePage>{
       print("리포트 불러오기 오류: $e");
     }
   }
+  Future<void> sendFeedback(int noteId, bool agree) async{
+    try{
+      final response = await http.post(Uri.parse('http://10.0.2.2:8080/note/report/feedback'),headers: {'Content-Type':'application/json'},body: jsonEncode({'noteId':noteId,'agree':agree}),);
+
+      if(response.statusCode == 200){
+        print('피드백 전송 성공');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("피드백이 전송되었습니다.")),
+        );
+      }else{
+        print("피드백 실패 : ${response.statusCode}");
+      }
+    }catch(e){
+      print("예외 발생 : $e");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     if(report == null){
@@ -55,6 +73,7 @@ class _AIAnalysisHomePageState extends State<AIAnalysisHomePage>{
         body: Center(child: CircularProgressIndicator()),
       );
     }
+
     
     final suggestions = report!['suggestion'].toString().split(' / ');
     final byCategory = report!['byCategory'];
@@ -73,15 +92,36 @@ class _AIAnalysisHomePageState extends State<AIAnalysisHomePage>{
           ...suggestions.map((s) => Padding(padding: EdgeInsets.symmetric(vertical: 4),child: Text("📍 $s"))),
           SizedBox(height: 20),
           Text("카테고리별 통계"),
-          ...byCategory.map<Widget>((cat) => Card(
+          ...byCategory.map<Widget>((cat) {
+            final flaggedItems = cat['flaggedItems']??[];
+            return  Card(
               margin: EdgeInsets.symmetric(vertical: 8),
-              child: ListTile(
-                title: Text(cat['category']),
-                subtitle: Text(
-                  "총 지출: ${cat['totalAmount']}원 | 이상소비: ${cat['anomalyCount']}건 | 과소비: ${cat['overspendingCount']}건"
-                ),
+              child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(cat['category'] ?? "카테고리 없음"),
+                        Text("총 지출 : ${cat['totalAmount']}원 | 이상소비: ${cat['anomalyCount']}건 | 과소비: ${cat['overspendingCount']}건"),
+                        SizedBox(height: 8),
+                        ...flaggedItems.map<Widget>((item) => Card(
+                          child: ListTile(
+                            title: Text("${item['content']?.isNotEmpty ==true ?item['content']: "내용 없음"} - ${item['amount'] ?? 0}원"),
+                            subtitle: Text("${item['date'] ?? "-"} /" "${item['isAnomaly'] == true ? '이상소비' : item['isOverspending'] == true ? '과소비' : ''}"),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(onPressed: () => sendFeedback(item['id'],true), icon: Icon(Icons.thumb_up),),
+                                IconButton(onPressed: () => sendFeedback(item['id'],false), icon: Icon(Icons.thumb_down),),
+                              ],
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
               ),
-        )),
+            );
+          }),
         ],
       ),
       // body: ListView.builder(
